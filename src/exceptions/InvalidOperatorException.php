ffc8ef259d9677c7d55639e38f8df260249a83b0
Lachezar Mihaylov <contact@lmihaylov.com>
2019-02-21 04:36:13 +0200

    Develop the following exceptions: InvalidOperator, InvalidProperty and InvalidValue.

---
+++ src/exceptions/InvalidOperatorException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ExpressionBuilder\Exceptions;
+
+/**
+ * Class InvalidOperatorException
+ * @package ExpressionBuilder\Exceptions
+ *
+ * The exception will be throw in case of invalid operator by expression parsing.
+ */
+class InvalidOperatorException extends BaseException
+{
+}
---
+++ src/exceptions/InvalidPropertyException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ExpressionBuilder\Exceptions;
+
+/**
+ * Class InvalidPropertyException
+ * @package ExpressionBuilder\Exceptions
+ *
+ * The exception will be throw in case of invalid property.
+ */
+class InvalidPropertyException extends BaseException
+{
+}
