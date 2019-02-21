9d7cc2f1ea70ac0a817681f1bcb0161e7d2094bf
Lachezar Mihaylov <contact@lmihaylov.com>
2019-02-21 04:35:21 +0200

    Create 'exceptions' package and base exception class.

---
+++ src/exceptions/BaseException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ExpressionBuilder\Exceptions;
+
+use \Exception as PhpException;
+
+/**
+ * Class BaseException
+ * @package ExpressionBuilder\Exceptions
+ *
+ * Represents the library base exception class.
+ */
+class BaseException extends PhpException
+{
+}
